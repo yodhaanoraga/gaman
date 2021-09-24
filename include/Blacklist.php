2111eb3cd6d1ce773830bcc7a48ec0ad6d768605
Harwinda Purnawan <gtab.ewin@gmail.com>
2021-09-24 21:24:43 +0700

    issues/23 - Modify Blacklist file

--- include/Blacklist.php
+++ include/Blacklist.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Arionum;
+namespace Gaman;
 
 /**
  * Class Blacklist
@@ -12,7 +12,6 @@ final class Blacklist
      */
     public const PUBLIC_KEYS = [
         // phpcs:disable Generic.Files.LineLength
-        'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCvVQcHHCNLfiP9LmzWhhpCHx39Bhc67P5HMQM9cctEFvcsUdgrkGqy18taz9ZMrAGtq7NhBYpQ4ZTHkKYiZDaSUqQ' => 'Faucet Abuser',
         'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCxYDeQHk7Ke66UB2Un3UMmMoJ7RF5vDZXihdEXi8gk8ZBRAi35aFrER2ZLX1mgND7sLFXKETGTjRYjoHcuRNiJN1g' => 'Octaex Exchange',
         // phpcs:enable
     ];
